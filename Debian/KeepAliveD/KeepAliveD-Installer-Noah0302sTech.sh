b1c05e87f61b14a6c5b0a1857fdeb00c2d9e1e0e
Noah0302sTech <Noah0302sTech@gmail.com>
2023-10-13 10:25:16 +0200

    Translated Script from German to English Changed Spinner-Delay to 0.25s Added echoEnd() and replaced multiple echo with Function Removed unnecessary -y argument after apt update Added additional Information when being promted for Custom-Values Updated shPrimaryVar to the current Name of the Bash-Cript

--- Debian/KeepAliveD/KeepAliveD-Installer-Noah0302sTech.sh
+++ Debian/KeepAliveD/KeepAliveD-Installer-Noah0302sTech.sh
@@ -5,7 +5,7 @@
 #---------- Initial Checks & Functions
 	#----- Check for administrative privileges
 		if [[ $EUID -ne 0 ]]; then
-			echo "Das Skript muss mit Admin-Privilegien ausgeführt werden! (sudo)"
+			echo "This Script needs to be run with Root-Privileges! (sudo)"
 			exit 1
 		fi
 
@@ -37,7 +37,7 @@
 						# start spinner
 						i=1
 						sp='\|/-'
-						delay=${SPINNER_DELAY:-0.15}
+						delay=${SPINNER_DELAY:-0.25}
 
 						while :
 						do
@@ -85,12 +85,20 @@
 
 
 
+		#----- echoEnd
+				function echoEnd {
+					echo
+					echo
+					echo
+				}
+
+
+
 	#----- Refresh Packages
 		start_spinner "Aktualisiere Package-Listen..."
-			sudo apt update -y > /dev/null 2>&1
+			sudo apt update > /dev/null 2>&1
 		stop_spinner $?
-		echo
-		echo
+		echoEnd
 
 #-----	-----#	#-----	-----#	#-----	-----#
 #-----	-----#	#-----	-----#	#-----	-----#
@@ -112,41 +120,40 @@
 
 #----- Prompt for custom values
 	#--- Interface Name
-		read -p "Gib den Interface-Namen an [default: $ifaceName]: " input
+		read -p "Enter the Interface-Name (Most likely the primary of this Machine) [default: $ifaceName]: " input
 		ifaceName=${input:-$ifaceName}
 
 	#--- Unicast Source
-		read -p "Gib die Unicast-Source-IP an [default: $uniSrc]: " input
+		read -p "Enter the Unicast-Source-IP (IP of this Machine) [default: $uniSrc]: " input
 		uniSrc=${input:-$uniSrc}
 
 	#--- Unicast Destination
-		read -p "Gib die Unicast-Destination-IP an [default: $uniPeer]: " input
+		read -p "Enter the Unicast-Destination-IP (IP of the other Machine) [default: $uniPeer]: " input
 		uniPeer=${input:-$uniPeer}
 
 	#--- Virtual IP
-		read -p "Gib die virtuelle KeepAliveD-IP an [default: $virtIP]: " input
+		read -p "Enter the virtual KeepAliveD-IP (The Highly-Availible-IP) [default: $virtIP]: " input
 		virtIP=${input:-$virtIP}
 
 	#--- Unbound PW
-		read -p "Gib das KeepAliveD-Passwort an [default: $keepAlivedPW]: " input
+		read -p "Enter the KeepAliveD-Password (Needs to be the same on both Machines) [default: $keepAlivedPW]: " input
 		keepAlivedPW=${input:-$keepAlivedPW}
 
 	#--- Priority
-		read -p "Gib die Priorität an (Höher=Primary) [default: $prio]: " input
+		read -p "Enter the Priority of this Machine (Higher=Primary, Lower=Secondary) [default: $prio]: " input
 		prio=${input:-$prio}
-	echo
-	echo
+	echoEnd
 
 
 
 #----- KeepAliveD
 	#--- Install KeepAliveD
-		start_spinner "Installiere KeepAliveD..."
+		start_spinner "Installing KeepAliveD..."
 			apt install keepalived -y > /dev/null 2>&1
 		stop_spinner $?
 
 	#--- Install KeepAliveD
-		start_spinner "Konfiguriere KeepAliveD-Config..."
+		start_spinner "Configuring KeepAliveD-Config..."
 			echo "" > /etc/keepalived/keepalived.conf
 			echo '#Primary
 vrrp_instance VI_1 {
@@ -172,16 +179,15 @@ vrrp_instance VI_1 {
 			stop_spinner $?
 
 	#--- Enable
-		start_spinner "Aktiviere KeepAliveD..."
+		start_spinner "Enabling KeepAliveD..."
 			systemctl enable --now keepalived.service > /dev/null 2>&1
 		stop_spinner $?
 
 	#--- Status
-		start_spinner "KeepAliveD Status..."
+		echo "KeepAliveD Status..."
 			systemctl status keepalived.service
-		stop_spinner $?
-	echo
-	echo
+			sleep 1
+	echoEnd
 
 
 
@@ -194,10 +200,10 @@ vrrp_instance VI_1 {
 #----- Variables
 	folderVar=Pihole
 	subFolderVar=KeepAlived
-	shPrimaryVar=KeepAlived-Installer.sh
+	shPrimaryVar=KeepAliveD-Installer-Noah0302sTech.sh
 
 #----- Create Folders
-	start_spinner "Erstelle Verzeichnisse..."
+	start_spinner "Creating Directories..."
 		#--- /home/$SUDO_USER/Noah0302sTech
 			if [ ! -d /home/$SUDO_USER/Noah0302sTech ]; then
 				mkdir /home/$SUDO_USER/Noah0302sTech > /dev/null 2>&1
@@ -221,7 +227,7 @@ vrrp_instance VI_1 {
 	stop_spinner $?
 
 #----- Move Bash-Script
-	start_spinner "Verschiebe Bash-Skript..."
+	start_spinner "Moving $shPrimaryVar..."
 		#--- Primary Script Variable
 			if [ ! -f /home/$SUDO_USER/Noah0302sTech/$folderVar/$subFolderVar/$shPrimaryVar ]; then
 				mv /home/$SUDO_USER/$shPrimaryVar /home/$SUDO_USER/Noah0302sTech/$folderVar/$subFolderVar/$shPrimaryVar > /dev/null 2>&1
